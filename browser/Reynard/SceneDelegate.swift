2e5846c88fd37e96ed6a3bf1aabd901684492d64
Zammer-eri <itsahmdxyt@gmail.com>
2026-05-28 22:44:07 +0200

    Update SceneDelegate.swift

--- browser/Reynard/SceneDelegate.swift
+++ browser/Reynard/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        ChatGPTWebsiteDataCleaner.clearLaunchCaches()
+        ChatGPTWebsiteDataCleaner.clearStaleLaunchCachesIfNeeded()
 
         // Start before the root view loads; viewDidLoad opens the first Gecko session.
         JITController.shared.start()
