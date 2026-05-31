244b4da33c1e02aeb439783c6d190dbcd3aa8639
Zammer-eri <itsahmdxyt@gmail.com>
2026-05-31 05:01:46 +0200

    Update create-ipa.sh

--- tools/release/create-ipa.sh
+++ tools/release/create-ipa.sh
@@ -24,11 +24,18 @@ if [ -z "$APP_PATH" ]; then
 	exit 1
 fi
 
-rm -rf "$WORK_DIR" "$ROOT_DIR/dist/Reynard-TrollStore.tipa"
+# I absolutely hate Apple for this
+# Why is my bundle identifier just become unavailable for no reason?
+plutil -replace CFBundleIdentifier -string "com.minh-ton.Reynard" "$APP_PATH/Info.plist"
+plutil -replace CFBundleIdentifier -string "com.minh-ton.Reynard.Helper" "$APP_PATH/PlugIns/Reynard Helper.appex/Info.plist"
+plutil -replace CFBundleIdentifier -string "com.minh-ton.Reynard.OpenIn" "$APP_PATH/PlugIns/OpenIn.appex/Info.plist"
+
+rm -rf "$WORK_DIR" "$ROOT_DIR/dist/Reynard.ipa" "$ROOT_DIR/dist/Reynard-TrollStore.tipa"
 mkdir -p "$WORK_DIR/Payload"
 cp -R "$APP_PATH" "$WORK_DIR/Payload/"
 
 cd "$WORK_DIR"
+zip -r ../Reynard.ipa Payload -x "._*" -x ".DS_Store" -x "__MACOSX" # normal ipa
 
 PTRACE_JIT_SRC="$ROOT_DIR/browser/Reynard/TrollStore/JIT/ptrace_jit.c"
 PTRACE_JIT_OUT="Payload/Reynard.app/ptrace_jit"
@@ -36,27 +43,14 @@ PTRACE_JIT_OUT="Payload/Reynard.app/ptrace_jit"
 "$CLANG_PATH" \
 	-arch arm64 \
 	-isysroot "$SDK_PATH" \
-	-miphoneos-version-min=14.0 \
+	-miphoneos-version-min=13.0 \
 	-Os \
 	"$PTRACE_JIT_SRC" \
 	-o "$PTRACE_JIT_OUT"
 
 chmod 0755 "$PTRACE_JIT_OUT"
-
-find Payload -type f -exec sh -c '
-	for file do
-		case "$(file -b "$file")" in
-			*Mach-O*)
-				ldid -S "$file"
-				;;
-		esac
-	done
-' sh {} +
-
 ldid -S"$ROOT_DIR/browser/Reynard/TrollStore/JIT/ptrace_jit.entitlements" "$PTRACE_JIT_OUT"
 ldid -S"$ROOT_DIR/browser/Reynard/Entitlements/Reynard.private.entitlements" "Payload/Reynard.app/Reynard"
 ldid -S"$ROOT_DIR/browser/Helper/Entitlements/Reynard-Helper.private.entitlements" "Payload/Reynard.app/PlugIns/Reynard Helper.appex/Reynard Helper"
-if [ -f "Payload/Reynard.app/PlugIns/OpenIn.appex/OpenIn" ]; then
-	ldid -S "Payload/Reynard.app/PlugIns/OpenIn.appex/OpenIn"
-fi
 zip -r ../Reynard-TrollStore.tipa Payload -x "._*" -x ".DS_Store" -x "__MACOSX" # trollstore ipa
+cp ../Reynard-TrollStore.tipa ../Reynard-Jailbroken.ipa # for jailbroken users
